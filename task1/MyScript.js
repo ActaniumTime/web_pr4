0158b3fb08215bc381afdc39dc903a5582f37a1a
Mashkov_Vol <spartan117987654321@gmail.com>
2024-02-28 01:21:00 +0100

    task1_compited

---
+++ task1/MyScript.js
@@ -0,0 +1,18 @@
+function Officer(name, department, phoneNumber, Salary){
+    this.name = name;
+    this.department = department;
+    this.phoneNumber = phoneNumber;
+    this.Salary = Salary;   
+}
+
+var officer = new Officer('Tom', 'IT department', '+380684967544', '4700');
+
+console.log('task1');
+console.log('Total info:');
+console.log(officer.name);
+console.log(officer.department);
+console.log(officer.Salary);
+console.log(officer.phoneNumber);
+
+officer.adress = 'skisna 74/76';
+
