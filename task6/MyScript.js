a6e8e15087ce8a01ee62cacb6491a57961798876
Mashkov_Vol <spartan117987654321@gmail.com>
2024-02-28 03:24:11 +0100

    All tasks is completed

---
+++ task6/MyScript.js
@@ -0,0 +1,23 @@
+function MenuItem(title, url) {
+    this.title = title;
+    this.url = url;
+}
+
+
+MenuItem.prototype.show = function() {
+    document.write("<ul>");
+    for (var i = 0; i < arguments.length; i++) {
+        document.write("<li><a href='" + arguments[i].url + "'>" + arguments[i].title + "</a></li>");
+    }
+    document.write("</ul>");
+};
+var count = parseInt(prompt("Введите количество пунктов меню:"));
+var menuItems = [];
+for (var i = 0; i < count; i++) {
+    var title = prompt("Введите название пункта меню " + (i + 1) + ":");
+    var url = prompt("Введите URL для пункта меню " + (i + 1) + ":");
+    menuItems.push(new MenuItem(title, url));
+}
+
+var menu = new MenuItem();
+menu.show.apply(null, menuItems);
